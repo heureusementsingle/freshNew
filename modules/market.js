55edd49d053a0710b08cf8ef1dd24f592ca7f994
heureusement <1181298985@qq.com>
2016-10-27 21:40:16 +0800

    modified beequick

--- modules/market.js
+++ modules/market.js
@@ -1,29 +1,88 @@
 define(["text!../views/market.html","baidu","zepto","$css!../css/market.css"],function(html){
 	function render(){
 		$(".container").html(html);
-		for(var i=0;i<$(".marketContent>ul li").length;i++){
-			(function(i){
-				Zepto($(".marketContent>ul li").get(i)).tap(function(){
-					$(this).css("border-left","0.15625rem solid #ffd600");
-					$($(".marketInner>ul").get(i)).css("display","block");
-					for(var j=0;j<$(".marketContent>ul li").length;j++){
-						if(j!=i){
-							$($(".marketContent>ul li").get(j)).css("border-left","none");
-							$($(".marketInner>ul").get(j)).css("display","none");
-						}
-					}
-				})
-			}(i));
-		}
 		$.ajax({
 			type:"get",
 			url:"http://10.3.156.235:8020/beequick/modules/category.json",
 			success:function(result){
 				var data=result.data;
 				var html=baidu.template("categoryData",data);
-				var Text=baidu.template("EveryProductList",data);
 				$(".marketContent>ul").html(html);
-				$(".marketInnerPro").html(Text);
+				for(var i=0;i<$(".marketContent>ul li").length;i++){
+					(function(i){
+						Zepto($(".marketContent>ul li").get(i)).tap(function(){
+							console.log(i);
+							$(this).css("border-left","0.15625rem solid #ffd600");
+							if($(this).text()=="热销榜"){
+								$.ajax({
+									type:"get",
+									url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E7%83%AD%E9%94%80%E6%A6%9C",
+									success:function(data){
+										var data=JSON.parse(data);
+										console.log(data);
+										var Text=baidu.template("EveryProductList",data);
+										$(".marketInner>ul").html(Text);
+									}
+								});
+							}else if($(this).text()=="天天特价"){
+								$.ajax({
+									type:"get",
+									url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E5%A4%A9%E5%A4%A9%E7%89%B9%E4%BB%B7",
+									success:function(data){
+										var data=JSON.parse(data);
+										var Text=baidu.template("EveryProductList",data);
+										$(".marketInner>ul").html(Text);
+										console.log(data);
+									}
+								});
+							}else if($(this).text()=="优选水果"){
+								$.ajax({
+									type:"get",
+									url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E4%BC%98%E9%80%89%E6%B0%B4%E6%9E%9C",
+									success:function(data){
+										var data=JSON.parse(data);
+										var Text=baidu.template("EveryProductList",data);
+										$(".marketInner>ul").html(Text);
+									}
+								})
+							}else if($(this).text()=="牛奶面包"){
+								$.ajax({
+									type:"get",
+									url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E4%BC%98%E9%80%89%E6%B0%B4%E6%9E%9C",
+									success:function(data){
+										var data=JSON.parse(data);
+										var Text=baidu.template("EveryProductList",data);
+										$(".marketInner>ul").html(Text);
+									}
+								})
+							}else{
+								$.ajax({
+									type:"get",
+									url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E7%89%9B%E5%A5%B6%E9%9D%A2%E5%8C%85",
+									success:function(data){
+										var data=JSON.parse(data);
+										var Text=baidu.template("EveryProductList",data);
+										$(".marketInner>ul").html(Text);
+									}
+								})
+							}
+							for(var j=0;j<$(".marketContent>ul li").length;j++){
+								if(j!=i){
+									$($(".marketContent>ul li").get(j)).css("border-left","none");
+								}
+							}
+						})
+					}(i));
+				}
+			}
+		});
+		$.ajax({
+			type:"get",
+			url:"http://www.vrserver.applinzi.com/aixianfeng/apicategory.php?category=%E7%83%AD%E9%94%80%E6%A6%9C",
+			success:function(data){
+				var data=JSON.parse(data);
+				var Text=baidu.template("EveryProductList",data);
+				$(".marketInner>ul").html(Text);
 			}
 		});
 		var flag=false;
